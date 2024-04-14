79b2c761ceaffada5ff679aa2a85c9c0599bc364
KsanStone <83030315+KsanStone@users.noreply.github.com>
2024-04-14 15:44:53 +0200

    made the ansi term condidional on windows

--- src/main.rs
+++ src/main.rs
@@ -64,8 +64,11 @@ fn main() {
             }
         }
     }).collect();
-
-    let _ = ansi_term::enable_ansi_support();
+    
+    #[cfg(target_os = "windows")]
+    {
+        let _ = ansi_term::enable_ansi_support();
+    }
 
     let encoding: Option<&'static Encoding> = if args.encoding == "auto" {
         None
